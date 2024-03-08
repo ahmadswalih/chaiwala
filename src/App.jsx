060fec4558436106a6779d129df1a2764745784e
ahmadswalih <ahmadswalih777@gmail.com>
2024-03-08 20:00:53 +0530

    updated logo , updated terms-and-conditions

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ const App = () => {
         <img
           src={logo}
           alt="OOPPSSS.. logo goes here "
-          className=" w-72 md:w-96 drop-shadow-md "
+          className=" w-60 md:w-72 drop-shadow-md "
           srcset=""
         />
         <p className=" text-4xl md:text-6xl font-bold mt-3 ">चायवाला </p>
@@ -65,6 +65,7 @@ const App = () => {
           The application will list the greatest things said by{" "}
           <span className="font-bold"> चायवाला</span>.
         </p>
+
         <div className="flex md:flex-row flex-col mt-10">
           <input
             onChange={(e) => {
@@ -100,6 +101,13 @@ const App = () => {
             ""
           )}
         </div>
+        <p className="mt-4 text-gray-600 text-lg">
+          By using this website , You are accepting our{" "}
+          <a href="#terms-and-conditions">
+            {" "}
+            <span className="text-blue-500 underline">Terms & Conditions </span>
+          </a>
+        </p>
         {result.length > 0 ? (
           ""
         ) : (
@@ -165,7 +173,18 @@ const App = () => {
               </div>
             ))}
         <Tags />
-
+        <a target="_blank" href="">
+          {" "}
+          <p className="bg-red-500 mt-3 text-white rounded-md p-2 text-xl mb-4">
+            GITHUB ➚{" "}
+          </p>
+        </a>
+        <a target="_blank" href="">
+          {" "}
+          <p className="bg-green-500 text-white rounded-md p-2 text-xl mb-4">
+            Contribute a Quote ➚{" "}
+          </p>
+        </a>
         <a
           target="_blank"
           href="https://us.umami.is/share/DTJgMGKtPwcQcCiG/trumplaughs.ahmadswalih.com"
@@ -181,6 +200,76 @@ const App = () => {
         >
           Logo courtesy <span className="text-red-700"> Sarkartoon</span>
         </a>
+
+        <div
+          id="terms-and-conditions"
+          className="flex items-center flex-col w-[44rem] p-3 rounded-md mb-10 shadow-md bg-gray-100 border border-gray-200"
+        >
+          <p className="text-red-600 text-xl mb-2 text-center font-bold">
+            Terms and conditions
+          </p>
+          <p className=" text-center">
+            Terms and Conditions These Terms and Conditions govern your use of
+            the website https://chaiwala.ahmadswalih.com, an open-source project
+            dedicated to compiling and displaying statements made by Prime
+            Minister Narendra Modi in public platforms. By accessing or using
+            the website, you agree to abide by these Terms and Conditions. If
+            you do not agree with any part of these terms, please refrain from
+            using the website. Ownership of Data: The data displayed on the
+            website is sourced from the internet and is not owned by us. We do
+            not claim ownership of the content displayed. If you believe that
+            any content infringes on your legal rights or copyright, please
+            contact us immediately, and we will take the necessary actions to
+            address the issue. Legal Compliance: We are committed to complying
+            with all applicable laws and regulations. If any legal or copyright
+            issues arise regarding the content displayed on the website, we will
+            cooperate fully with relevant authorities and stakeholders to
+            resolve the matter promptly and appropriately. Content will be
+            removed if it is proven to be inaccurately attributed to Prime
+            Minister Narendra Modi or lacks proper authorization. Direct
+            Communication: Users or individuals with concerns about the website
+            are encouraged to contact us directly through email at [insert email
+            address]. We value open communication and are committed to
+            addressing any issues promptly and transparently. Non-Defamation and
+            Non-Harassment: The purpose of this website is to provide a platform
+            for information dissemination and public discourse. We do not
+            endorse or condone any form of defamation or harassment against
+            individuals or entities mentioned on the website. Users are
+            prohibited from using the website to engage in defamatory,
+            harassing, or unlawful activities. Political Neutrality: The website
+            maintains a stance of political neutrality and impartiality. We do
+            not have any affiliations or connections with any political parties,
+            organizations, or individuals. The content displayed on the website
+            is presented objectively and without bias. User Conduct: Users of
+            the website are expected to conduct themselves in a respectful and
+            lawful manner. Any misuse of the website, including but not limited
+            to unauthorized access, data manipulation, or dissemination of
+            offensive content, is strictly prohibited. We reserve the right to
+            suspend or terminate user accounts found to be in violation of these
+            Terms and Conditions. Disclaimer of Warranties: While we strive to
+            provide accurate and up-to-date information, we make no warranties
+            or representations regarding the accuracy, reliability, or
+            completeness of the content displayed on the website. Users access
+            the website at their own risk and discretion. Limitation of
+            Liability: In no event shall we be liable for any direct, indirect,
+            incidental, special, or consequential damages arising out of or in
+            any way connected with the use of the website or the inability to
+            use the website, even if we have been advised of the possibility of
+            such damages. Modification of Terms: We reserve the right to modify
+            or update these Terms and Conditions at any time without prior
+            notice. By continuing to use the website after such changes are
+            made, you acknowledge and agree to be bound by the revised terms.
+            Governing Law: These Terms and Conditions shall be governed by and
+            construed in accordance with the laws of right to speech. Any
+            disputes arising out of or relating to these terms shall be subject
+            to the exclusive jurisdiction of the courts in right to act and
+            right to speech. Contact Information: If you have any questions or
+            concerns about these Terms and Conditions, please contact us at
+            ahmadswalih777@gmail.com. By using this website, you acknowledge
+            that you have read, understood, and agree to be bound by these Terms
+            and Conditions. Thank you for your cooperation and understanding.
+          </p>
+        </div>
       </div>
     </div>
   );
